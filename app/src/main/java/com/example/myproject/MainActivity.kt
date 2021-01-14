251a3da375fa4674f5dd9ff35d3cf034859589fe
tpyc12 <tpyc12@yandex.ru>
2021-01-14 18:08:20 +0300

    HW

--- app/src/main/java/com/example/myproject/MainActivity.kt
+++ app/src/main/java/com/example/myproject/MainActivity.kt
@@ -1,14 +1,20 @@
-package com.example.myproject;
+package com.example.myproject
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import com.example.myproject.R
 
-import android.os.Bundle;
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-public class MainActivity extends AppCompatActivity {
+        val button = findViewById<Button>(R.id.buttonPushMe)
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        button.setOnClickListener {
+            (it as Button).text = "Done"
+        }
     }
 }
